8054397aa39a5bd4f32431791fefeeb8926868fe
Gladwinnnn <gladwin1503@hotmail.com>
2022-06-29 12:40:28 +0800

    cors error resolved

--- src/main/java/com/TestBlog/controller/PostController.java
+++ src/main/java/com/TestBlog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.TestBlog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.TestBlog.entity.Post;
 import com.TestBlog.service.PostService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PostController {
 	
 	@Autowired
